20dcba2d423d93a2f8d10155f2be0e21d68741d9
FGtatsuro <fujiistorage@gmail.com>
2016-04-13 18:24:59 +0900

    Use Bash as login shell on spec testing.

--- spec/anyenv_spec.rb
+++ spec/anyenv_spec.rb
@@ -1,6 +1,9 @@
 require "spec_helper_#{ENV['SPEC_TARGET_BACKEND']}"
 
-describe command('anyenv install ndenv && exec $SHELL -l') do
+set :shell, '/bin/bash'
+set :login_shell, true
+
+describe command('anyenv install ndenv') do
   its(:exit_status) { should eq 0 }
 end
 
