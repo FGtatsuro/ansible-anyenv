600c9fa780d5313e3ad0a47911da84705c2daba9
FGtatsuro <fujiistorage@gmail.com>
2016-04-13 18:45:54 +0900

    Load profile in each description.

--- spec/anyenv_spec.rb
+++ spec/anyenv_spec.rb
@@ -1,12 +1,9 @@
 require "spec_helper_#{ENV['SPEC_TARGET_BACKEND']}"
 
-set :shell, '/bin/bash'
-set :login_shell, true
-
-describe command('anyenv install ndenv') do
+describe command('exec /bin/bash -l && anyenv install ndenv') do
   its(:exit_status) { should eq 0 }
 end
 
-describe command('which ndenv') do
+describe command('exec /bin/bash -l && which ndenv') do
   its(:exit_status) { should eq 0 }
 end
