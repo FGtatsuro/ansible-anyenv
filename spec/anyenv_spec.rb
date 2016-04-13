0657a29de55208d2956530290e8e16a1742c63a9
FGtatsuro <fujiistorage@gmail.com>
2016-04-13 17:52:05 +0900

    Update spec.
    Install ndenv && check whether ndenv is on PATH.

--- spec/anyenv_spec.rb
+++ spec/anyenv_spec.rb
@@ -1,37 +1,9 @@
 require "spec_helper_#{ENV['SPEC_TARGET_BACKEND']}"
 
-describe command('which renv') do
-  its(:exit_status) { should eq 0 }
-end
-
-describe command('which plenv') do
-  its(:exit_status) { should eq 0 }
-end
-
-describe command('which pyenv') do
-  its(:exit_status) { should eq 0 }
-end
-
-describe command('which phpenv') do
+describe command('anyenv install ndenv && exec $SHELL -l') do
   its(:exit_status) { should eq 0 }
 end
 
 describe command('which ndenv') do
-  its(:exit_status) { should eq 0 }
-end
-
-describe command('which denv') do
-  its(:exit_status) { should eq 0 }
-end
-
-describe command('which jenv') do
-  its(:exit_status) { should eq 0 }
-end
-
-describe command('which luaenv') do
-  its(:exit_status) { should eq 0 }
-end
-
-describe command('which goenv') do
   its(:exit_status) { should eq 0 }
 end
