fa0c31779986856280026e2f1204987841790e91
FGtatsuro <fujiistorage@gmail.com>
2016-04-14 15:02:59 +0900

    Fix specs on OSX.
    `exec /bin/bash -l` causes hang on OSX.

--- spec/anyenv_spec.rb
+++ spec/anyenv_spec.rb
@@ -1,9 +1,17 @@
 require "spec_helper_#{ENV['SPEC_TARGET_BACKEND']}"
 
-describe command('exec /bin/bash -l && anyenv install ndenv') do
+describe command('exec /bin/bash -l && anyenv install ndenv'), :if => ['debian', 'alpine'].include?(os[:family]) do
   its(:exit_status) { should eq 0 }
 end
 
-describe command('exec /bin/bash -l && which ndenv') do
+describe command('exec /bin/bash -l && which ndenv'), :if => ['debian', 'alpine'].include?(os[:family]) do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('source /Users/travis/.profile && anyenv install ndenv'), :if => os[:family] == 'darwin' do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('source /Users/travis/.profile && which ndenv'), :if => os[:family] == 'darwin' do
   its(:exit_status) { should eq 0 }
 end
